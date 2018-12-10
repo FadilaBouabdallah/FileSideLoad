7967b92c9f813ab0e8118a739e6508997341b05c
FadilaBouabdallah <43574438+FadilaBouabdallah@users.noreply.github.com>
2018-12-10 22:05:28 +0100

    Add files via upload

---
+++ config/module.config.php
@@ -0,0 +1,23 @@
+<?php
+namespace FileSideload;
+
+return [
+    'media_ingesters' => [
+        'factories' => [
+            'sideload' => Service\MediaIngesterSideloadFactory::class,
+        ],
+    ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
+    'csv_import_media_ingester_adapter' => [
+        'sideload' => CSVImport\SideloadMediaIngesterAdapter::class,
+    ],
+];
